f2d420ff02abd1c22ee98d718fa0610bcd89c329
Gloinfur <gloinfur@gmail.com>
2022-12-12 12:48:27 +0100

    Challenge 1: refactored

--- 1/Bash/1.sh
+++ 1/Bash/1.sh
@@ -6,35 +6,46 @@ file_inputFile="./input.txt"
 index=0
 calories=0
 
-#--- LOOP ---#
-while IFS= read -r line
-do
-	if [ ! -z $line ]
-	then
-		calories=$(($calories + $line))
-	elif [ -z $line ]
-	then
-		(( index += 1 ))
-		arr_elvCalories[$index]=$calories
-		calories=0
-	fi
-done < "$file_inputFile"
+#--- FUNCTIONS ---#
+getHighestValue() {
+	tmpArray=("$@")
+	highestValue=$(printf '%s\n' "${tmpArray[@]}" | awk '$1 > m || NR == 1 { m = $1 } END { print m }')
+	echo $highestValue
+}
+
+deleteValueFromArray() {
+	tmpArray=("$@")
+	tmpArray=( "${tmpArray[@]/$2}" )
+}
+
+getValuesFromFile() {
+	while IFS= read -r line
+	do
+        	if [ ! -z $line ]
+        	then
+                	calories=$(($calories + $line))
+        	elif [ -z $line ]
+        	then
+                	(( index += 1 ))
+                	arr_elvCalories[$index]=$calories
+                	calories=0
+        	fi
+	done < "$file_inputFile"	
+}
 
 #--- RESULTS ---#
-caloriesTopOne=$(printf '%s\n' "${arr_elvCalories[@]}" | awk '$1 > m || NR == 1 { m = $1 } END { print m }')
-echo "Top 1:"
-echo $caloriesTopOne
+getValuesFromFile
+
+caloriesTopOne=$(getHighestValue "${arr_elvCalories[@]}")
+echo "Top 1: $caloriesTopOne"
 
 arr_elvCalories=( "${arr_elvCalories[@]/$caloriesTopOne}" )
-caloriesTopTwo=$(printf '%s\n' "${arr_elvCalories[@]}" | awk '$1 > m || NR == 1 { m = $1 } END { print m }')
-echo "Top 2:"
-echo $caloriesTopTwo
+caloriesTopTwo=$(getHighestValue "${arr_elvCalories[@]}")
+echo "Top 2: $caloriesTopTwo"
 
 arr_elvCalories=( "${arr_elvCalories[@]/$caloriesTopTwo}" )
-caloriesTopThree=$(printf '%s\n' "${arr_elvCalories[@]}" | awk '$1 > m || NR == 1 { m = $1 } END { print m }')
-echo "Top 3:"
-echo $caloriesTopThree
+caloriesTopThree=$(getHighestValue "${arr_elvCalories[@]}")
+echo "Top 3: $caloriesTopThree"
 
 total=$(($caloriesTopOne + $caloriesTopTwo + $caloriesTopThree))
-echo "Top 3 elves in total:"
-echo $total
+echo "Total: $total"
